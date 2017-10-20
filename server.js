6bf9178bbdfa981a13609dbd0ccd75d6a731586f
Tim Noorlander <tim@Tims-iMac.local>
2017-10-20 09:40:41 +0200

    trying to implement breakpad symbols

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'),
 multer = require('multer'),
-bodyParser = require('body-parser'),
+// bodyParser = require('body-parser'),
 path = require('path'),
 fs = require('fs'),
 http = require('http'),
@@ -14,7 +14,6 @@ const app = express(),
 Raven.config('https://8b71cd6f4174477bb676af0180606e95:249b0addee054517982a153247ef9092@sentry.io/226455').install();
 
 app.use(Raven.requestHandler());
-
 
 const upload = multer({
     dest: miniDumpsPath
@@ -30,20 +29,34 @@ function handleStackTrace(error, report) {
 }
 
 app.post('/desktop/crash-report', upload, function (req, res) {
-    // minidump.walkStack(req.file.path, handleStackTrace);
+    // minidump.walkStack(req.file.path, __dirname + '/electron.breakpad.syms', handleStackTrace);
+
     req.body.filename = req.file.filename;
     const crashLog = JSON.stringify(req.body, undefined, 2);
 
     fs.writeFile(req.file.path + '.json', crashLog, function (err) {
         if (err) return console.error('Error saving crash report: ' + err.message);
-        console.log('Saved crash report:\n\t' + crashLog);
+        // console.log('Saved crash report:\n\t' + crashLog);
+        // console.log(req.file.path + '.json');
     });
     //TODO: minidump meesturen
     // Raven.context({
     //     extra: {file: req.file}
     // });
 
-    Raven.captureException(crashLog);
+    // var error = new Error('Orbit Desktop crash on version ' + req.body.packageVersion);
+    var error = new Error();
+    error.message ='Orbit Desktop crash on version ' + req.body.packageVersion;
+    // error.stacktrace = fs.readFileSync(req.file.path);
+    // error.fileName = req.file.path + '.json';
+    // const error = {
+    //     message: 'Orbit Desktop crash on version ' + crashLog.appVersion,
+    //     fileName: req.file.path + '.json'
+    // };
+
+    console.log(error);
+
+    Raven.captureException(error);
 
     res.end()
 });
@@ -55,7 +68,7 @@ app.use(function onError(err, req, res) {
     res.end(res.sentry + '\n');
 });
 
-server.listen(3010
-    , () => {
-    console.log('running on port 3010');
+
+server.listen(3000, function() {
+    console.log('running on port 3000');
 });
